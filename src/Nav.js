5b2f6ad466c90825c616963274d679782fffced3
Himanshu Singh Parmar <hp2146@gmail.com>
2021-02-08 03:13:15 +0530

    initial commit 8

--- src/Nav.js
+++ src/Nav.js
@@ -22,7 +22,7 @@ function Nav() {
       <div className="nav_contents">
         <img
           className="nav_logo"
-          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          src="https://www.freepnglogos.com/uploads/red-netflix-logo-text-png-3.png"
         />
         <img
           className="nav_avatar"
